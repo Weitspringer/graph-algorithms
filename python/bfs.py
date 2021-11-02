c611e7f7b9b8e7103e9a102362a83fe82c8d66e7
Weitspringer <luca_springer@yahoo.com>
2021-11-02 21:25:54 +0100

    Change datastructure for queue in simple BFS Change examplary graph

--- python/bfs.py
+++ python/bfs.py
@@ -1,20 +1,20 @@
-from queue import Queue
+from collections import deque
 
 def bfs(adjacency_list, node_key):
     # adjacency_list is of form {"a": [<keys>], "b": [<keys>], ...}
     b = {}
-    q = Queue()
-    q.put(node_key)
+    q = deque()
+    q.append(node_key)
     num = 1
     b.update({node_key: num})
-    while not q.empty():
-        current_key = q.get()
+    while q:
+        current_key = q.pop()
         for i in adjacency_list[current_key]:
             if i not in b:
                 num += 1
-                q.put(i)
+                q.append(i)
                 b.update({i: num})
     return b
 
-adj = {"a": ["b", "c"], "b": ["c","d"], "c": [], "d": ["a"]}
+adj = {"a": ["b", "c"], "b": ["a","c","d"], "c": ["a", "b"], "d": ["b"]}
 print(bfs(adj, "a"))
