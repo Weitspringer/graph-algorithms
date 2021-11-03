04dd5b3dea5c2847ec498403a5fbc81db23a574b
Weitspringer <luca_springer@yahoo.com>
2021-11-03 17:56:21 +0100

    Add some output for main and test

--- python/main.py
+++ python/main.py
@@ -2,5 +2,6 @@ import src
 from test.tests import TestBasicAlgorithms
 
 if __name__ == "__main__":
+    print("Running tests...")
     basic = TestBasicAlgorithms()
     basic.test_simple_bfs()
--- python/test/tests.py
+++ python/test/tests.py
@@ -5,11 +5,16 @@ from src.dfs import simple_dfs
 class TestBasicAlgorithms(unittest.TestCase):
 
     def test_simple_bfs(self):
-        adj_1 = {"a": ["b", "c"], "b": ["a", "c", "d"], "c": ["a", "b"], "d": ["b"]}
+        adj_1 = {"a": ["b", "c"], "b": ["a", "c", "d"], "c": ["a", "b"], "d": ["b"]} # same as undirected graph
+        print("Adjacency matrix: {}".format(adj_1))
         b_1_a = simple_bfs(adj_1, "a")
+        print("Simple BFS from a: {}".format(b_1_a))
         b_1_b = simple_bfs(adj_1, "b")
+        print("Simple BFS from b: {}".format(b_1_b))
         b_1_c = simple_bfs(adj_1, "c")
+        print("Simple BFS from c: {}".format(b_1_c))
         b_1_d = simple_bfs(adj_1, "d")
+        print("Simple BFS from d: {}".format(b_1_d))
         self.assertTrue(b_1_a["a"] == 1 and b_1_b["b"] == 1 and b_1_c["c"] == 1 and b_1_d["d"] == 1)
         self.assertLess(b_1_a["a"], b_1_a["b"])
         self.assertLess(b_1_a["a"], b_1_a["c"])
@@ -17,6 +22,7 @@ class TestBasicAlgorithms(unittest.TestCase):
         self.assertLess(b_1_a["c"], b_1_a["d"])
         self.assertEqual(b_1_a["d"], max(b_1_a.values()))
         adj_2 = {"a": ["b", "c", "d"], "b": ["e"], "c": ["f"], "d": [], "e": ["g"], "f": [], "g": []}
+        print("Tests for simple BFS passed!")
 
     def test_simple_dfs(self):
         return
